ecc2d878cf372ca215be4d848a84b84710571a4f
Alexandr Veremeenko <asver88@gmail.com>
2014-02-05 16:04:41 +0200

    Added more helper methods

--- VAHelper/VAHelper.h
+++ VAHelper/VAHelper.h
@@ -14,11 +14,27 @@
 + (BOOL)isIpad;
 + (BOOL)isIphone;
 
++ (BOOL)is4inchScreen;
++ (BOOL)is3_5inchScreen;
++ (BOOL)isRetinaScreen;
+
++ (NSString *)deviceName;
++ (NSString *)deviceModel;
++ (NSString *)deviceSystemName;
++ (NSString *)deviceSystemVersionString;
++ (float)deviceSystemVersionFloatValue;
+
 + (BOOL)isSystemVersionLessThanIOS7;
 + (BOOL)isSystemVersionLessThanIOS6;
 
 + (BOOL)isPortrait;
 + (BOOL)isLandscape;
 + (UIInterfaceOrientation)interfaceOrientation;
+
++ (NSString *)applicationName;
++ (NSString *)applicationVersion;
++ (NSString *)applicationBuildVersion;
++ (NSString *)applicationDefaultUserAgent;
++ (NSDictionary *)applicationInfoDictionary;
 
 @end
--- VAHelper/VAHelper.m
+++ VAHelper/VAHelper.m
@@ -11,8 +11,6 @@
 
 @implementation VAHelper
 
-#pragma mark - UIDevice information helper methods
-
 + (BOOL)isIpad
 {
     return ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad);
@@ -23,14 +21,58 @@
     return ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone);
 }
 
+#pragma mark - Screen information
+
++ (BOOL)is4inchScreen
+{
+    return ([UIScreen mainScreen].bounds.size.height == 568.);
+}
+
++ (BOOL)is3_5inchScreen
+{
+    return ([UIScreen mainScreen].bounds.size.height == 480.);
+}
+
++ (BOOL)isRetinaScreen
+{
+    return ([[UIScreen mainScreen] scale] == 2);
+}
+
+#pragma mark - Device information
+
++ (NSString *)deviceName
+{
+    return [UIDevice currentDevice].name;
+}
+
++ (NSString *)deviceModel
+{
+    return [UIDevice currentDevice].model;
+}
+
++ (NSString *)deviceSystemName
+{
+    return [UIDevice currentDevice].systemName;
+}
+
++ (NSString *)deviceSystemVersionString
+{
+    return [UIDevice currentDevice].systemVersion;
+}
+
++ (float)deviceSystemVersionFloatValue
+{
+    return [[VAHelper deviceSystemVersionString] floatValue];
+}
+
 + (BOOL)isSystemVersionLessThanIOS7
 {
-    return ([[UIDevice currentDevice].systemVersion floatValue] < 7.0);
+    return ([VAHelper deviceSystemVersionFloatValue] < 7.0);
 }
 
 + (BOOL)isSystemVersionLessThanIOS6
 {
-    return ([[UIDevice currentDevice].systemVersion floatValue] < 6.0);
+    return ([VAHelper deviceSystemVersionFloatValue] < 6.0);
 }
 
 #pragma mark - UIInterfaceOrientation helper methods
@@ -49,6 +91,36 @@
 + (UIInterfaceOrientation)interfaceOrientation
 {
     return [[UIApplication sharedApplication] statusBarOrientation];
+}
+
+#pragma mark - Application information
+
++ (NSString *)applicationName
+{
+    NSDictionary *infoDictionary = [VAHelper applicationInfoDictionary];
+    return infoDictionary[@"CFBundleDisplayName"];
+}
+
++ (NSString *)applicationVersion
+{
+    NSDictionary *infoDictionary = [VAHelper applicationInfoDictionary];
+    return infoDictionary[@"CFBundleShortVersionString"];
+}
+
++ (NSString *)applicationBuildVersion
+{
+    NSDictionary *infoDictionary = [VAHelper applicationInfoDictionary];
+    return infoDictionary[@"CFBundleVersion"];
+}
+
++ (NSString *)applicationDefaultUserAgent
+{
+    return @"";//TODO: complete it!
+}
+
++ (NSDictionary *)applicationInfoDictionary
+{
+    return [[NSBundle mainBundle] infoDictionary];
 }
 
 @end
